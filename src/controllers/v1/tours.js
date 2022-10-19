f38245d6770a9de54b56dad521503199a599daad
pmfoysal <pmfoysal@gmail.com>
2022-10-19 22:05:22 +0600

    GET /api/v1/tours API Completed

--- src/controllers/v1/tours.js
+++ src/controllers/v1/tours.js
@@ -1,22 +1,52 @@
 const tours = require('@services').v1.tours;
 
-exports.getTours = async (req, res, nex) => {};
+exports.getTours = async (req, res, nex) => {
+   try {
+      const result = await tours?.getTours(req?.query);
+      res.status(200).send({
+         status: 'Success',
+         message: 'Performed the Action!',
+         data: result,
+      });
+   } catch (error) {
+      res.status(400).send({
+         status: 'Failed',
+         message: 'Something Went Wrong!',
+         error: error?.message,
+      });
+   }
+};
 
 exports.postTours = async (req, res, nex) => {
    try {
       const result = await tours?.postTours(req?.body);
       res.status(200).send({
-         status: 200,
-         message: 'Successfully Performed the Action',
+         status: 'Success',
+         message: 'Performed the Action!',
          data: result,
       });
-   } catch (e) {
+   } catch (error) {
       res.status(400).send({
-         status: 400,
+         status: 'Failed',
          message: 'Something Went Wrong!',
-         error: e?.message,
+         error: error?.message,
       });
    }
 };
 
-exports.getTour = async (req, res, nex) => {};
+exports.getTour = async (req, res, nex) => {
+   try {
+      const result = await tours?.getTour(req?.params?.id, req?.query?.fields);
+      res.status(200).send({
+         status: 'Success',
+         message: 'Performed the Action!',
+         data: result,
+      });
+   } catch (error) {
+      res.status(400).send({
+         status: 'Failed',
+         message: 'Something Went Wrong!',
+         error: error?.message,
+      });
+   }
+};
--- src/services/v1/tours.js
+++ src/services/v1/tours.js
@@ -1,5 +1,24 @@
 const tours = require('@models').v1.tours;
 
+exports.getTours = async queries => {
+   const fields = queries?.fields?.replaceAll(/[\, ]/g, ' ');
+   const start = queries?.limit * (queries?.page - 1);
+   const data = await tours
+      ?.find({ name: { $regex: queries?.search, $options: 'i' }, ...queries?.filter }, fields)
+      .sort(queries?.sort);
+
+   return {
+      totalItems: data?.length,
+      totalPages: Math.ceil(data?.length / (Number(queries?.limit) || data?.length)),
+      data: data?.slice(start, start + Number(queries?.limit)),
+   };
+};
+
 exports.postTours = async data => {
-   return await tours.create(data);
+   return await tours?.create(data);
+};
+
+exports.getTour = async (id, fields) => {
+   const queries = fields?.replaceAll(/[\, ]/g, ' ');
+   return await tours?.findById(id, queries);
 };
